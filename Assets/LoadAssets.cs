a56d0ed7975c5abc5a071671b0d82afc9dc4ebda
oujianjiu <oujianjiu@gmail.com>
2020-08-24 03:52:17 +1000

    add blueObj
    70% done

--- Assets/LoadAssets.cs
+++ Assets/LoadAssets.cs
@@ -6,10 +6,12 @@ public class LoadAssets : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject redObj;
-    public GameObject blueObj;
+    [SerializeField]
+    private GameObject blueObj;
     void Start()
     {
-        
+        Instantiate(redObj,new Vector3(2.0f,0,0),Quaternion.identity);
+        Instantiate(blueObj, new Vector3(-2.0f, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
